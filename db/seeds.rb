9b538b1a3face459c3d9ba36f824b533d9f4ee05
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-25 13:28:05 +0000

    working seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create!([
+    {
+        name: "Stuart McKenzie",
+        image: "http://placehold.it/80x80"
+    },    
+    {
+        name: "Elle Tweedy",
+        image: "http://placehold.it/80x80"
+    },    
+    {
+        name: "Chris Evans",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Chris Wensley",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Jenny Thai",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Ashley O'Callaghan",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Ashley Evans",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Dom Freeman",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Jaye Hackett",
+        image: "http://placehold.it/80x80"
+    },
+    {
+        name: "Joe Bramall",
+        image: "http://placehold.it/80x80"
+    },
+])
+
+40.times do |i|
+    Compliment.create(
+        :person => Person.order("RANDOM()").first,
+        :body => "Pretty great",
+        :attribution => "No"
+    )
+end
