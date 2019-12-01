63af2a8ed5f4dafa6e40e286dc317da18a540e0d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-01 19:28:04 +0000

    whoops, route was wrong

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :people, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :people, controllers: { omniauth_callbacks: 'people/omniauth_callbacks' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
